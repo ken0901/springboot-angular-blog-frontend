918343c4bc49f7b23a78d69c31d029adb1d34931
ken0901 <ken5260715@gmail.com>
2021-09-11 00:00:11 -0700

    Add register

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,27 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './component/header/header.component';
 import { HomeComponent } from './component/home/home.component';
+import { RegisterComponent } from './component/auth/register/register.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    HomeComponent
+    HomeComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    RouterModule.forRoot([
+      {path:'register', component: RegisterComponent}
+    ]),
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/service/auth.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { RegisterPayload } from '../component/auth/register-payload';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  private url = 'http://localhost:8080/'
+
+  constructor(private httpClient: HttpClient) { }
+
+  register(registerPayload: RegisterPayload): Observable<any> {
+    return this.httpClient.post(this.url+'signup',registerPayload);
+  }
+}
